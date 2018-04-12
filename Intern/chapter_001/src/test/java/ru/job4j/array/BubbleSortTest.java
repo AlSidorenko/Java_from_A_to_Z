bd8b81a867ae96d7ce262781df467da0c5541899
AlSidorenko <alek.sidorenko1979@gmail.com>
2018-04-12 18:46:35 +0300

    Bubble Sort

--- Intern/chapter_001/src/test/java/ru/job4j/array/BubbleSortTest.java
+++ Intern/chapter_001/src/test/java/ru/job4j/array/BubbleSortTest.java
@@ -12,16 +12,16 @@ import static org.junit.Assert.assertThat;
  * @version $Id$
  * @since 0.1
  */
- public class BubbleSortTest {
-	/**
+public class BubbleSortTest {
+    /**
      * Test Turn Array.
      */
     @Test
     public void whenSortArrayWithTenElementsThenSortedArray() {
-		BubbleSort bs = new BubbleSort();
-		int[] a = {1, 5, 4, 2, 3, 1, 7, 8, 0, 5};
-		int[] resultArray = bs.sort(a);
-		int[] expectedArray = {0, 1, 1, 2, 3, 4, 5, 5, 7, 8};
-		assertThat(resultArray, is(expectedArray));
+        BubbleSort bs = new BubbleSort();
+        int[] a = {1, 5, 4, 2, 3, 1, 7, 8, 0, 5};
+        int[] resultArray = bs.sort(a);
+        int[] expectedArray = {0, 1, 1, 2, 3, 4, 5, 5, 7, 8};
+        assertThat(resultArray, is(expectedArray));
     }
 }
