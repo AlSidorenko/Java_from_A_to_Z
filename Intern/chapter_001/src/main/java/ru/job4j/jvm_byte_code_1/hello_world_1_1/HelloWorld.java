6c1f9492c81c9140990c2f9e4707f21885ec090a
AlSidorenko <alek.sidorenko1979@gmail.com>
2018-08-02 17:31:23 +0300

    1.1. Создание Hello World [#141]

---
+++ Intern/chapter_001/src/main/java/ru/job4j/jvm_byte_code_1/hello_world_1_1/HelloWorld.java
@@ -0,0 +1,19 @@
+package ru.job4j.jvm_byte_code_1.hello_world_1_1;
+
+/**
+ * Created on 02.08.2018.
+ *
+ * @author Aleks Sidorenko (alek.sidorenko1979@gmail.com).
+ * @version $Id$.
+ * @since 0.1.
+ */
+public class HelloWorld {
+
+    /**
+     * Main method.
+     * @param args - String array.
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello world.");
+    }
+}
